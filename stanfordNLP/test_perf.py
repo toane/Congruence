0e0022c62f7fef6e48f0f82bfb55ea81a8f20a06
sapristi <megabigfoutoir@gmail.com>
2018-05-09 11:55:14 +0200

    integration corenlp

--- stanfordNLP/test_perf.py
+++ stanfordNLP/test_perf.py
@@ -2,14 +2,11 @@
 from stanfordcorenlp import StanfordCoreNLP
 from timeit import default_timer as timer
 
-# lancement du coreNLP par le script python
-nlp = StanfordCoreNLP(r'/home/mathias/bigdata/projet/Congruence/stanfordNLP/bin')
-
-nlp_srv = StanfordCoreNLP('http://localhost', port=9000)
 
 
-text = """
-France's Foreign Minister Jean-Yves Le Drian says the Iranian nuclear deal is "not dead" despite US President Donald Trump's decision to withdraw.
+
+text_raw = """
+France's Foreign Minister Jean-Yves Le Drian says the Iranian nuclear deal is "not dead" despite US President Donald Trump's decision to withdraw. 
 
 The 2015 agreement curbed Iran's nuclear activities in return for the lifting of sanctions that had been imposed by the UN, US and EU.
 
@@ -19,20 +16,32 @@ Other signatories to the nuclear accord say they remain committed to it.
 
 The deal was agreed between Iran and the five permanent members of the UN Security Council - the US, UK, France, China and Russia - plus Germany. It was struck under Mr Trump's predecessor, Barack Obama.
 """
+text = text_raw.split( ".")
+
+
+
+nlp = StanfordCoreNLP(r'/home/mathias/bigdata/projet/Congruence/stanfordNLP/bin')
 
 start = timer()
-nlp.ner(text)
-nlp.pos_tag(text)
+for i in range(100):
+    map( lambda sentence : nlp.ner(sentence), text)
+    map( lambda sentence : nlp.pos_tag(sentence), text)
 end = timer()
+inline_time = end - start
 
-print("Time for inline NLP : ", start-end)
+nlp.close()
+
+nlp_srv = StanfordCoreNLP('http://localhost', port=9000)
 
 
 start = timer()
-nlp_srv.ner(text)
-nlp_srv.pos_tag(text)
+for i in range(100):
+    map( lambda sentence : nlp_srv.ner(sentence), text)
+    map( lambda sentence : nlp_srv.pos_tag(sentence), text)
 end = timer()
+srv_time = end - start
 
 
-print("Time for server NLP : ", start-end)
+print("Time for inline NLP : ", inline_time)
+print("Time for server NLP : ", srv_time)
 
