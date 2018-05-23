8d5bf70bc38bc4579a6bad437e68fefc049e80b7
malakoff550 <malakoff550>
2018-05-23 12:52:14 +0200

    ajout de mute_print() dans utils/muteprint.py

---
+++ utils/muteprint.py
@@ -0,0 +1,16 @@
+"""
+Usage: importer dans la classe dont les prints doivent etre redefinis
+ajouter les instructions:
+from utils.printdebug import mute_print
+print = mute_print(print)
+"""
+
+DEBUG = True
+
+def mute_print(func):
+    def wrapped_func(*args,**kwargs):
+        if DEBUG is True:
+            return func(*args, **kwargs)
+    return wrapped_func
+
+
