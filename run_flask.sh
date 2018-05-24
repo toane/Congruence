74dc9cc141a0c676278468a0c946a22e367cb0ae
sapristi <megabigfoutoir@gmail.com>
2018-05-24 16:59:49 +0200

    stuff

--- run_flask.sh
+++ run_flask.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "http://localhost:5000/run"
 export FLASK_ENV=development
-FLASK_APP=app.py flask run
+FLASK_APP=app.py flask run --host 192.168.1.53
 #python -m flask run --host 192.168.1.53
