8aff27bf4f6723cc434005a1d9f15216cd7ad547
sapristi <megabigfoutoir@gmail.com>
2018-05-25 14:08:17 +0200

    flask host fix

--- run_flask.sh
+++ run_flask.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 echo "http://localhost:5000/run"
 export FLASK_ENV=development
-FLASK_APP=app.py flask run --host 127.0.0.1
-#python -m flask run --host 192.168.1.53
+FLASK_APP=app.py flask run  --host 0.0.0.0
